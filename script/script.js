8ed7ee745926c5240735c85db7b75497b99875cb
Ntandou Sacre <sacrentandou2.0@gmail.com>
2024-04-18 04:25:11 +0200

    reglage du menu

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 const header = document.querySelector(".navbar")
+const navbarToggle = document.querySelector(".navbar-toggler");
 
+navbarToggle.addEventListener("click", function() {
+    if (window.scrollY < 100) {
+        header.classList.toggle("navbarLight");
+    }
+});
 window.onscroll = function() {
     var top = window.scrollY;
     if(top >=100) {
